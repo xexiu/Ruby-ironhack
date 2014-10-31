24f7db14db408e8acdfeb2d997fd5108cd2f55aa
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-10-31 10:53:07 +0100

    Sinatra v2

--- new_sinatra/sinatra-tutorial.rb
+++ new_sinatra/sinatra-tutorial.rb
@@ -1,15 +1,19 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 get '/' do
+  @date = DateTime.now
+  @bucle = 0
   erb :index
 end
 
 post '/hello' do
-  @name = params["username"]
+  @name = params[:username]
   "Hello #{@name}"
+  # redirect to "/hello/#{@name}"
 end
 
 get '/hello/:username' do
-  @name = params["username"]
+  @name = params[:username]
   "Hello #{@name}"
 end
